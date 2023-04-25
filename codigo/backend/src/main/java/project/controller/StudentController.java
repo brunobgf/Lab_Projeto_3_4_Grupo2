d00fadb5e10b7bfda617371bc348c2d7c2591216
MatheusMOAndrade <andradematheusmo@gmail.com>
2023-04-25 03:23:30 -0300

    student controller and service update

--- codigo/backend/src/main/java/project/controller/StudentController.java
+++ codigo/backend/src/main/java/project/controller/StudentController.java
@@ -1,5 +1,55 @@
 package project.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import project.model.Student;
+import project.service.StudentService;
+
+@RestController
+@RequestMapping("/student")
+@Validated
 public class StudentController {
 
+    @Autowired
+    private StudentService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> get(@PathVariable Long id) {
+        return ResponseEntity.ok().body(service.get(id));
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<Student>> getAll() {
+        return ResponseEntity.ok().body(service.getAll());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Boolean> put(@PathVariable Long id, @RequestBody Student obj) {
+        return ResponseEntity.ok().body(service.put(id, obj));
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Boolean> post(@RequestBody Student obj) {
+        return ResponseEntity.ok().body(service.post(obj));
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+
+    }
 }
--- codigo/backend/src/main/java/project/service/StudentService.java
+++ codigo/backend/src/main/java/project/service/StudentService.java
@@ -1,5 +1,45 @@
 package project.service;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import project.model.Student;
+import project.repository.StudentRepository;
+
+@Service
 public class StudentService {
+
+    @Autowired
+    private StudentRepository repository;
+
+    public Student get(Long id) {
+        return repository.findById(id).get();
+    }
+
+    public List<Student> getAll() {
+        return repository.findAll();
+    }
+
+    @Transactional
+    public boolean put(Long id, Student obj) {
+        obj.setId(id);
+        return repository.save(obj) != null;
+    }
+
+    @Transactional
+    public boolean post(Student obj) {
+        obj.setId(null);
+        return repository.save(obj) != null;
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        repository.deleteById(id);
+
+    }
 
 }
