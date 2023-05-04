3fc64fc21299e1b37e35cb7af1d62de92d3784e7
MatheusMOAndrade <andradematheusmo@gmail.com>
2023-05-04 19:50:33 -0300

    database update

--- codigo/backend/src/main/resources/db/migration/v1.0_CreateTable.sql
+++ codigo/backend/src/main/resources/db/migration/v1.0_CreateTable.sql
@@ -1,20 +1,37 @@
-CREATE TABLE student (
-                       id INT auto_increment PRIMARY KEY,
-                       name VARCHAR(100) NOT NULL,
-                       cpf VARCHAR(15) NOT NULL,
-                       rg VARCHAR(15) NOT NULL,
-                       institute VARCHAR(30) NOT NULL,
-                       course VARCHAR(30) NOT NULL,
-                       email VARCHAR(15) NOT NULL,
-                       login VARCHAR(20) NOT NULL unique,
-                       password VARCHAR(20) NOT NULL
-);
+CREATE TABLE coin (
+    id INT auto_increment PRIMARY KEY,
+    motivation VARCHAR(50) NOT NULL,
+
+    CONSTRAINT FK_for_student_coin_id FOREIGN KEY (student_id) REFERENCES student(id)
+)
 
 CREATE TABLE partner (
-                       id INT auto_increment PRIMARY KEY,
-                       name VARCHAR(100) NOT NULL,
-                       cnpj VARCHAR(15) NOT NULL,
-                       email VARCHAR(15) NOT NULL,
-                       login VARCHAR(20) NOT NULL unique,
-                       password VARCHAR(20) NOT NULL
+    id INT auto_increment PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    cnpj VARCHAR(15) NOT NULL,
+    email VARCHAR(15) NOT NULL,
+    login VARCHAR(20) NOT NULL unique,
+    password VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE professor (
+    id INT auto_increment PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    cpf VARCHAR(15) NOT NULL,
+    departament VARCHAR(30) NOT NULL,
+    institution VARCHAR(30) NOT NULL,
+    login VARCHAR(20) NOT NULL unique,
+    password VARCHAR(20) NOT NULL
+)
+
+CREATE TABLE student (
+    id INT auto_increment PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    cpf VARCHAR(15) NOT NULL,
+    rg VARCHAR(15) NOT NULL,
+    institute VARCHAR(30) NOT NULL,
+    course VARCHAR(30) NOT NULL,
+    email VARCHAR(15) NOT NULL,
+    login VARCHAR(20) NOT NULL unique,
+    password VARCHAR(20) NOT NULL
 );
